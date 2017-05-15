a30a08253d0d7b60bd76b915b5febed7117c91e5
Marsel <marselvalitov@gmail.com>
2017-05-15 11:52:14 +0300

    save count

--- lib/pinger/importer.ex
+++ lib/pinger/importer.ex
@@ -9,21 +9,27 @@ defmodule Pinger.Importer do
 
   def init(id) do
     Logger.debug "#{inspect(self())} IMPORTER init #{inspect(id)}"
-    {:producer_consumer, id, subscribe_to: [{Pinger.Loader, max_demand: 1, min_demand: 0}]}
+    {:ok, conn} = Redix.start_link(host: Application.get_env(:pinger, :redis_host), port: Application.get_env(:pinger, :redis_port))
+    {:producer_consumer, conn, subscribe_to: [{Pinger.Loader, max_demand: 1, min_demand: 0}]}
   end
 
-  def handle_events([group], from, id) do
-    Logger.debug "#{inspect(self())} IMPORTER handle_events #{inspect(group)} from #{inspect(from)}, #{inspect(id)}"
+  def handle_events([group], from, conn) do
+    Logger.debug "#{inspect(self())} IMPORTER handle_events #{inspect(group)} from #{inspect(from)}, #{inspect(conn)}"
     proxies =
       Downloader.download(group)
       |> generate_proxies(group.id)
-    {:noreply, proxies, id}
+    save_count(conn, Enum.count(proxies), group.id)
+    {:noreply, proxies, conn}
   end
 
   defp generate_proxies list, id do
     for l <- list do
       %Pinger.Proxy{group_id: id, url: l}
     end
+  end
+
+  defp save_count(conn, count, group_id) do
+    Redix.command(conn, ["SET", "proxy|count|group_#{group_id}", count])
   end
 
 end
